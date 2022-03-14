fba929ea83ae9d7bce2a201333b00679f8248cf7
MasseRaphael <masseraphael76@gmail.com>
2022-03-14 14:58:24 +0100

    🚀 RELEASE: ajout du bruteforce

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import bruteForce from "./bruteforce.js";
 import air from "./circle.js";
 import fibonacci from "./fibonacci.js";
 import sunday from "./sunday.js";
@@ -5,3 +6,4 @@ import sunday from "./sunday.js";
 console.log(air(2));
 console.log(sunday());
 console.log(fibonacci(8));
+console.log(bruteForce("Bonjour", 10));
